77d6e6f9d3489032c9200f80871fd22ecab05928
Adomas-Grigorovicius <adomasgrigorovicius@gmail.com>
2026-04-15 13:54:02 +0300

    Added class structure

---
+++ simulation/agent.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+import random
+import simulation.grid import Grid
+
+
+class BaseAgent():
+    def __init__(self, grid: Grid):
+        
+
+        
+
+class RandomAgent(BaseAgent):
+    def move(self):
+
+
+
+
+class GreedyAgent(BaseAgent):
+    def move(self):
+
+
