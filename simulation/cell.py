e1c4f3ae7b94e15d5cfe4b40eb922de9e105c8d1
Adomas-Grigorovicius <adomasgrigorovicius@gmail.com>
2026-04-24 14:15:00 +0300

    Fixed some minor issues

--- simulation/cell.py
+++ simulation/cell.py
@@ -1,38 +1,35 @@
 class Cell:
     VALID_TYPES = ("E","W","G","S")
 
-    def __init__(self, x: int, y: int, cell_type: str):
+    def __init__(self, x, y, cell_type):
         self.__x = x
         self.__y = y
         self.__cell_type = cell_type 
-    
+
     @property
-    def get_x(self):
+    def x(self):
         return self.__x
     
     @property
-    def get_y(self):
+    def y(self):
         return self.__y
     
     @property
-    def get_type(self):
+    def cell_type(self):
         return self.__cell_type
     
     @cell_type.setter
-    def set_type(self, cell_type):
-        if cell_type not in self.VALID_TYPES:
-            raise ValueError(f"Invalid cell type: {cell_type}")
-        self.__cell_type = cell_type
+    def cell_type(self, value):
+        if value not in self.VALID_TYPES:
+            raise ValueError(f"Invalid cell type: {value}")
+        self.__cell_type = value
     
-    @cell_type.setter
     def is_walkable(self: bool):
         return self.__cell_type != "W"
     
-    @cell_type.setter
     def is_goal(self: bool):
         return self.__cell_type == "G"
     
-    @cell_type.setter
     def __repr__(self: str):
         return f"Cell({self.__x}, {self.__y}, '{self.__cell_type}')"
         
