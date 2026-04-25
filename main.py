b461c4ff7d10f6942506ba4be06daf7628c160ea
Adomas-Grigorovicius <adomasgrigorovicius@gmail.com>
2026-04-25 15:43:48 +0300

    Fixed inccorect name of atribute

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ def run_simulation(agent_type: str):
     print(f"\nStarting simulation with {agent_type.capitalize()}Agent")
     print(f"Goal: ({grid.goal.x}, {grid.goal.y})\n")
 
-    grid.display(agent.x, agent.y)
+    grid.display(agent._x, agent._y)
 
     while not agent.has_reached_goal():
         agent.move()
-        grid.display(agent.x, agent.y)
+        grid.display(agent._x, agent._y)
 
     print(f"Goal reached in {agent.steps} steps!")
     file_manager.save_result(agent_type, agent.steps)
